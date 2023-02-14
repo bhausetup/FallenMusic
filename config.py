25a1942f8eea4bb19e39e602ffc9aaf773d319f8
bhausetup <109501363+bhausetup@users.noreply.github.com>
2023-02-15 04:13:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "23854524"))
+API_HASH = getenv("API_HASH", "e765ba734e8c485b1b1f72dff94127de")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "6152203566:AAEWiQZIsvs5iA8LtKblIaqdac_u_1VF4xk")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", "1952625698"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/7f8088007deb906b12a43.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/377541d1a70e239248b71.jpg")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", "BQDGWGUmJtcR58x-UYJTUTTobMZ0JxFneRFpqJ6GEI4YjbLKJPequSc2AAxQTutsRBEpeSeNK3uhsKY2GCaQ5B_RTVs6TDnIZ3s-a_lk4EJwoYrfkhIXqxMb9TBeZYNTWVp8-1Wm2NTo4TIVV9cBCMUw_ANbH7AzESBtaFS4CqLs_1Or_0DHbzn4gEqowzutrbSFsbs39UgaCRP7FKxZjCN0_FypHevvHCI8-R2uPdNlcE-Qwg52scSgne5v-kSpJjOVxnfGyl7alYgrWi5nVUR28uqtYMERqPOzKf1m5MO8NPG893fDRoKIEoW0fypvBlhRt5YGw7KyiaAtyuYHXScfAAAAAU3end8A")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ISHQ00_I")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/XD_CUTETY")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5322208820").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://telegra.ph/file/e899283ee1a944e6225a2.jpg"
